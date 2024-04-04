7e621d485adfcb47e458946ada858e86ba06f6ea
aman1348 <asinghchuphal@gmail.com>
2024-04-04 17:30:21 +0530

    trying again with initial package.json

--- tripforge_frontend/babel.config.js
+++ tripforge_frontend/babel.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-    presets: [
-        "@babel/preset-env",
-        "@babel/preset-react"
-    ],
-    plugins: [
-        ...require('@babel/plugin-proposal-private-property-in-object').default,
-        require('@babel/plugin-proposal-private-methods').default
-    ]
-};
+// module.exports = {
+//     presets: [
+//         "@babel/preset-env",
+//         "@babel/preset-react"
+//     ],
+//     plugins: [
+//         ...require('@babel/plugin-proposal-private-property-in-object').default,
+//         require('@babel/plugin-proposal-private-methods').default
+//     ]
+// };
